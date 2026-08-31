508bd74ce1f6e475c1f997f62d8d2dc212466f90
Magos Algorithmus <os.jhonatan@gmail.com>
2026-08-31 15:15:54 +0000

    Deploy GitHub Pages

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/scriptor-algorithmus/sw-share.js', { scope: '/scriptor-algorithmus/' })})}
